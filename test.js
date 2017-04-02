c5c15723f4ef9903e14584f2fde8b485347d1059
shamandrey@gmail.com <qwerty337208>
2017-04-02 15:46:54 +0300

    Refactoring sintax

--- test.js
+++ test.js
@@ -1,30 +1,31 @@
-var list = [                               
-"onmessage = function(e) {",
-  "var result = '';",
-  "for(var i=0;i<1000000;i++){result = result+i;}",
+var list = [
+    "onmessage = function(e) {",
+    "var result = '';",
+    "for(var i=0;i<1000000;i++){result = result+i;}",
     "postMessage({e:e.data[0]});}"
 ];
 
-var fanctionBitch = (function(v){
-    
+var fanctionBitch = (function () {
+
     var i = 0;
 
-    return function(event) {
-        if(i === 10){
-             worker.postMessage([Date.now()]);
-             i=0;
-        }else{
+    return function () {
+        if (i === 10) {
+            worker.postMessage([Date.now()]);
+
+            i = 0;
+        } else {
             i++;
         }
     }
-})()
+})();
 
 var blob = new Blob(list, {type: 'text/javascript'});
 
 var worker = new Worker(URL.createObjectURL(blob));
 
-worker.onmessage = function(e) {  
-  console.log('Message received from worker', Date.now() - e.data['e']);
-}
+worker.onmessage = function (e) {
+    console.log('Message received from worker', Date.now() - e.data['e']);
+};
 
 document.querySelector(".ol-unselectable").addEventListener("mousemove", fanctionBitch);
